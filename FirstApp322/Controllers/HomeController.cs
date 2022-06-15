2eeaada1a802909a5aa8b58f87ad4540e15f1587
CavadovT <turalkhj@code.edu.az>
2022-06-16 03:01:28 +0400

    Add project files.

---
+++ FirstApp322/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp322.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult GetInfo(int id)
+        {
+            return Json(new
+            {
+                Id=$"Id={id}",
+                name = "Tural",
+                surname = "Javadov",
+                Age = 28
+
+            });
+
+
+        }
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
